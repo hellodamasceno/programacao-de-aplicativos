f02458ae14399fb041dc8955fd77fc0ddbe87a8d
Helloyse damasceno <helloyse.damasceno01114988@sesisenaipr.org.br>
2026-02-19 10:04:24 -0300

    primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 16 # variável inteira
+peso = 60.1 # variável float
+nome = 'Helloyse Damasceno' # variável string
+sexo_feminino = True # variável bool 
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)
