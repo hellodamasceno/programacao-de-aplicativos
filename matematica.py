bbecf7d5d300aedf2dd2194df1275e1edef132d6
helloyse.damasceno01114988@sesisenaipr.org.br <helloyse.damasceno01114988@sesisenaipr.org.br>
2026-02-20 09:50:48 -0300

    gostei da aula

---
+++ matematica.py
@@ -0,0 +1,13 @@
+seja_bem_vindos = input("digite o seu nome: ")
+valor_inteiro1 = int(input("digite o primeiro valor: "))
+valor_inteiro2 = int(input("digite o segundo valor: "))
+
+soma = valor_inteiro1 + valor_inteiro2
+subtracao = valor_inteiro1 - valor_inteiro2
+divisao = valor_inteiro1 / valor_inteiro2
+multiplicacao = valor_inteiro1 * valor_inteiro2
+
+print("o resultado da soma é:", soma)
+print("o resultado da subtracao é:",subtracao)
+print("o resultado da divisao é:", divisao)
+print("o resutado da multiplicacao é:" , multiplicacao)
