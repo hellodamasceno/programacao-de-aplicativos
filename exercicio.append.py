d49b97b67427eeade43d8715b540420d85f65633
hello <helloyse.damasceno01114988@sesisenaipr.org.br>
2026-03-18 12:41:45 -0300

    aula dia 18/03

---
+++ exercicio.append.py
@@ -0,0 +1,5 @@
+lista = ["Ana", "Bruno", "Caio"]
+print(lista)
+
+lista.append("Daniel")
+lista.extend("Zuleica", [0])
---
+++ lista.cores.py
@@ -0,0 +1,4 @@
+cores =  ["Vermelho", "Verde", "Azul", "Amarelo", "Preto"]
+usuario = int(input(digite um numero de 0 a 4: ))
+
+print(f"O numero escolhido resultou a cor {cores[usuario]}")
