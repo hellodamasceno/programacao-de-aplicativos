27b8be6d5a6918e7e3a6c814fee8d7cb72d973a5
helloyse <helloyse.damasceno01114988@sesisenaipr.org.br>
2026-08-05 12:16:57 -0300

    continuacao revisao

--- Revisoes/revisao02.py
+++ Revisoes/revisao02.py
@@ -2,7 +2,7 @@ import sqlite3
 
 def criar_tabela():
     try:
-        conexao = sqlite3.connect("hospital.db")
+        conexao = sqlite3.connect("cinema.db")
         cursor = conexao.cursor()
         cursor.execute("PRAGMA foreign_keys = ON;")
         cursor.execute ('''CREATE TABLE IF NOT EXISTS cinemas 
@@ -11,10 +11,11 @@ def criar_tabela():
                         shopping TEXT
                         )''')
         cursor.execute ('''CREATE TABLE IF NOT EXISTS salas
-                        (id_cinema INTEGER PRIMARY KEY AUTOINCREMENT,
-                        numero TEXT,
+                        (id_sala INTEGER PRIMARY KEY AUTOINCREMENT,
+                        numero_sala TEXT,
                         capacidade INTEGER,
-                        FOREIGN KEY (id_cinema) REFERENCES cinemas(id)
+                        id_cinema INTEGER, 
+                        FOREIGN KEY (id_cinema) REFERENCES cinemas(id_cinema)
                     )''')
         conexao.commit()
     finally:
@@ -22,27 +23,52 @@ def criar_tabela():
 
 
 
-def cadastrar(numero, capacidade, id_cinema):
+def cadastrar_sala(numero_sala, capacidade, id_cinema):
     try:
-        conexao = sqlite3.connect("hospital.db")
+        conexao = sqlite3.connect("cinema.db")
         cursor = conexao.cursor()
         cursor.execute("PRAGMA foreign_keys = ON;")
         cursor.execute('''INSERT INTO salas
                        (numero_sala, capacidade, id_cinema) VALUES (?, ?, ?)''', (numero_sala, capacidade, id_cinema))
         conexao.commit()
-        print(f"Sala{numero} cadastrada com sucesso! ")
+        print(f"Sala {numero_sala} cadastrada com sucesso! ")
     except sqlite3.IntegrityError:
         print("Sala inexistente")
     finally:
         conexao.close()
 
-def listar():
+def cadastrar_cinema(nome, shopping):
     try:
-        conexao = sqlite3.connect("hospital.db")
+        conexao = sqlite3.connect("cinema.db")
+        cursor = conexao.cursor()
+        cursor.execute("INSERT INTO cinemas (nome, shopping) VALUES (?, ?)", (nome, shopping))
+        conexao.commit()
+        print("Cinema cadastrado com sucesso!")
+    finally:
+        conexao.close()
+
+def listar_cinemas():
+    try:
+        conexao = sqlite3.connect("cinema.db")
+        cursor = conexao.cursor()
+        print("\nLista dos cinemas:")
+        cursor.execute("SELECT id_cinema, nome, shopping FROM cinemas")
+        todos = cursor.fetchall()
+        if todos:
+            for c in todos:
+                print(f"ID: {c[0]} | Nome: {c[1]} | Shopping: {c[2]}")
+        else:
+            print("Nenhum cinema cadastrado.")
+    finally:
+        conexao.close()
+
+def listar_sala():
+    try:
+        conexao = sqlite3.connect("cinema.db")
         cursor = conexao.cursor()
         cursor.execute("PRAGMA foreign_keys = ON;")
         print("Lista das salas ")
-        cursor.execute(''' SELECT id, numero_sala, capacidade, id_cinema FROM salas''')
+        cursor.execute(''' SELECT id_sala, numero_sala, capacidade, id_cinema FROM salas''')
         todas_salas = cursor.fetchall()
         if todas_salas:
             for sala in todas_salas:
@@ -55,21 +81,28 @@ def listar():
 def menu():
     while True:
         print("-----Sistema de cinema-----")
-        print("1-CADASTRAR | 2-LISTAR | 3-SAIR")
+        print("1-CADASTRAR CINEMAS | 2-LISTAR CINEMAS | 3-CADASTRAR SALAS | 4-LISTAR SALAS | 3-SAIR")
         opcao = int(input("Digite a opção: "))
         if opcao == 1 :
-            print("----1 CADASTRO-----")
+            print("----1 CADASTRO CINEMA-----")
+            cidade = input("Digite a cidade: ")
+            shopping = input("Digite o nome do shopping: ")
+            cadastrar_cinema(cidade, shopping)
+
+        elif opcao == 2:
+            print("----2 LISTA DE CINEMAS----")
+            listar_cinemas()
+
+        elif opcao == 3:
+            print("-------3 CADASTRO DE SALA-------")
             numero = int(input("Digite o numero da sala: "))
             capacidade = int(input("Digite a capacidade da sala: "))
             id_cinema = int(input("Digite o ID do cinema: "))
-            cadastrar(numero, capacidade, id_cinema)
-
-        elif opcao == 2:
-            print("----2 LISTA DE SALAS----")
-            listar()
-        elif opcao == 3:
-            print("Saindo do sistema...") 
-            break 
+            cadastrar_sala(numero, capacidade, id_cinema)
+        elif opcao == 4:
+            print("------4 LISTA DE SALAS ")
+            listar_sala()
+            
 
 criar_tabela()  
 menu()
